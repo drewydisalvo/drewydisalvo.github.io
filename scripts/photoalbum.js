4ae53817f53c510d5046cad4114df9dfd73f2220
mmerendino@fordham.edu <mmerendino@fordham.edu>
2017-09-18 13:36:53 -0400

    Fixed dropdown on mobile, setup photography page.

---
+++ scripts/photoalbum.js
@@ -0,0 +1,29 @@
+function openItaly(){
+    var fileName = "images/Photography/Italy_"
+    var tempFileName;
+    var HTMLCode = '<div class="mySlides">\n <div class="numbertext">1 /24 </div> \n<img src="' + fileName+ '01.JPG " style="width:100%"> \n</div>\n';
+    var modal = document.getElementById("modalContent");
+
+    for(var i = 2; i < 24; i++){
+        if(i < 10){
+            tempFileName = fileName + 0 + i;
+        }
+        else{
+            tempFileName = fileName + i;
+        }
+        HTMLCode = HTMLCode + '<div class="mySlides">\n <div class="numbertext">' + i + '/24</div> \n<img src="' + tempFileName+ '.JPG " style="width:100%"> \n</div>\n'
+    }
+    modal.innerHTML = HTMLCode + '<a class="prev" onclick="plusSlides(-1)">&#10094; Prev.</a>\n<a class="next" onclick="plusSlides(1)">Next. &#10095;</a>\n <div class="caption-container">\n<p id="caption"></p>\n</div>';
+    openModal();
+    currentSlide(1);
+
+}
+function closePhotography(){
+    var modal = document.getElementById("modalContent");
+  
+    closeModal();
+   modal.innerHTML = "";  
+}
+function openJapan(){
+
+}
