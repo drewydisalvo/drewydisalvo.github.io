deef50647ee67c698c391a231e86d6d6498b03a4
mmerendino@fordham.edu <mmerendino@fordham.edu>
2017-09-18 14:56:15 -0400

    Added Japan

--- scripts/photoalbum.js
+++ scripts/photoalbum.js
@@ -25,5 +25,26 @@ function closePhotography(){
    modal.innerHTML = "";  
 }
 function openJapan(){
+    var fileName = "images/Photography/Japan_"
+    var tempFileName;
+    var HTMLCode = "";
+    var modal = document.getElementById("modalContent");
+
+    for(var i = 1; i < 7; i++){
+        if(i < 10){
+            tempFileName = fileName + 0 + i;
+        }
+        else{
+            tempFileName = fileName + i;
+        }
+        HTMLCode = HTMLCode + '<div class="mySlides">\n <div class="numbertext">' + i + '/6</div> \n<img src="' + tempFileName+ '.jpg " style="width:100%"> \n</div>\n'
+    }
+    /*for(var i = 29; i < 37; i++){
+        tempFileName = fileName + i;
+        HTMLCode = HTMLCode + '<div class="mySlides">\n <div class="numbertext">' + i + '/24</div> \n<img src="' + tempFileName+ '.RAF " style="width:100%"> \n</div>\n'
+    }*/
+    modal.innerHTML = HTMLCode + '<a class="prev" onclick="plusSlides(-1)">&#10094; Prev.</a>\n<a class="next" onclick="plusSlides(1)">Next. &#10095;</a>\n <div class="caption-container">\n<p id="caption"></p>\n</div>';
+    openModal();
+    currentSlide(1);
 
 }
