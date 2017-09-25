3bf1c9ee4574d162430c043f3ab062284fa25853
mmerendino@fordham.edu <mmerendino@fordham.edu>
2017-09-25 17:11:23 -0400

    Writing Samples page change

---
+++ scripts/writing.js
@@ -0,0 +1,7 @@
+var viewer = document.getElementById("sample");
+function glassgames(){
+    viewer.src = "downloads/GlassGamesDrewyPDF.pdf"
+}
+function typesandstripes(){
+    viewer.src = "downloads/TypesAndStripesDrewyPDF.pdf"
+}
