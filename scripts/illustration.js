baa9a6efbf4dca1084b15ef03e29847bd602dada
mmerendino@fordham.edu <mmerendino@fordham.edu>
2017-09-25 12:43:41 -0400

    illustration script

--- scripts/illustration.js
+++ scripts/illustration.js
@@ -2,7 +2,6 @@ var imgArray = [document.getElementById("i1"), document.getElementById("i2"),
 document.getElementById("i3"), document.getElementById("i4"), document.getElementById("i5"),
 document.getElementById("i6")]
 var imagePath = "images/Illustration/thumbnails/Illustrate_"
-pageNum = 0;
 index = 7;
 function rightbttn(){    
     for(var i =0; i < imgArray.length; i++){
@@ -15,36 +14,40 @@ function rightbttn(){
         index++;
         if(index == 33){
             index = 1; 
-            pageNum = 0;
         }
     }
     setslide()
-    pageNum++;
 }
 function setslide(){
-    imgArray[0].onclick = function onclick(event) { openModal(); currentSlide(1 + (6*pageNum)) }        
-    imgArray[1].onclick = function onclick(event) { openModal(); currentSlide(2 + (6*pageNum)) }        
-    imgArray[2].onclick = function onclick(event) { openModal(); currentSlide(3 + (6*pageNum)) }        
-    imgArray[3].onclick = function onclick(event) { openModal(); currentSlide(4 + (6*pageNum)) }        
-    imgArray[4].onclick = function onclick(event) { openModal(); currentSlide(5 + (6*pageNum)) }        
-    imgArray[5].onclick = function onclick(event) { openModal(); currentSlide(6 + (6*pageNum)) }
+    imgArray[0].onclick = function onclick(event) { openModal(); currentSlide(pullNum(imgArray[0].src)) }        
+    imgArray[1].onclick = function onclick(event) { openModal(); currentSlide(pullNum(imgArray[1].src)) }        
+    imgArray[2].onclick = function onclick(event) { openModal(); currentSlide(pullNum(imgArray[2].src)) }      
+    imgArray[3].onclick = function onclick(event) { openModal(); currentSlide(pullNum(imgArray[3].src)) }    
+    imgArray[4].onclick = function onclick(event) { openModal(); currentSlide(pullNum(imgArray[4].src)) }       
+    imgArray[5].onclick = function onclick(event) { openModal(); currentSlide(pullNum(imgArray[5].src)) } 
+}
+function pullNum(src){
+    var num;
+    var str;
+    str = src.charAt(106) + src.charAt(107);
+    num = parseInt(str);
+    return num;
 }
 function leftbttn(){
-    index = index - 6;
-    for(var i =0; i < imgArray.length; i++){
-        if(index == -1){
-            index = 33
-            pageNum = 1;            
+    function leftbttn(){
+        index = index - 12;
+        if(index <= 0){
+            index = 33 + index;
         }
-        if(index < 10){
-            imgArray[i].src = imagePath + "0" + index.toString() +"-min.jpg"
-        }  
-        else{
-            imgArray[i].src = imagePath + index.toString() +"-min.jpg"
+        for(var i =0; i < imgArray.length; i++){
+            if(index < 10){
+                imgArray[i].src = imagePath + "0" + index.toString() +"-min.jpg"
+            }  
+            else{
+                imgArray[i].src = imagePath + index.toString() +"-min.jpg"
+            }
+            index++;
         }
-        index--;
-
+        setslide();
     }
-    setslide();
-    pageNum--;
 }
