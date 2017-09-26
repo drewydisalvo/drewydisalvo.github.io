09a5679cd02fe9ca3f4b1545f5dd90a83cb5525f
mmerendino@fordham.edu <mmerendino@fordham.edu>
2017-09-25 20:07:14 -0400

    Updated writing page with download button, and fixed navbar.

--- scripts/writing.js
+++ scripts/writing.js
@@ -1,7 +1,15 @@
 var viewer = document.getElementById("sample");
+var downloadBttn = document.getElementById("downloadbttn");
 function glassgames(){
-    viewer.src = "downloads/GlassGamesDrewyPDF.pdf#toolbar=0"
+    viewer.src = "downloads/GlassGamesDrewyPDF.pdf#toolbar=0";
+    downloadBttn.style.visibility = "visible";
+    
 }
 function typesandstripes(){
-    viewer.src = "downloads/TypesAndStripesDrewyPDF.pdf#toolbar=0"
+    viewer.src = "downloads/TypesAndStripesDrewyPDF.pdf#toolbar=0";
+    downloadBttn.style.visibility = 'visible';  
+    
+}
+function download(){
+    window.open(viewer.src);
 }
