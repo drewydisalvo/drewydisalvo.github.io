b2810c8e1dfa8dee899892e4af7b372df1b605b3
mmerendino@fordham.edu <mmerendino@fordham.edu>
2017-09-27 17:28:54 -0400

    temp push

--- scripts/illustration.js
+++ scripts/illustration.js
@@ -1,22 +1,26 @@
-numOfImages = 33
+var numOfImages = 33
 
 var imgArray = [document.getElementById("i1"), document.getElementById("i2"), 
 document.getElementById("i3"), document.getElementById("i4"), document.getElementById("i5"),
 document.getElementById("i6")]
-var imagePath = "images/Illustration/thumbnails/Illustrate_"
-index = 7;
+var imagePath = "images/Illustration/Illustrate_"
+var thumbnailPath = "images/Illustration/thumbnails/Illustrate_"
+var index = 7;
 var modal = document.getElementById("popupContent")
 
-window.onload = function fillPopup(){
-    var path = "images/Illustration/Illustrate_"
+window.onload = function load(){
+    loadAllSlides();
+}
+
+function loadAllSlides(){
     var chunk = "";
     var currentHTML = modal.innerHTML;
     for(var i = 1; i < numOfImages+1; i++){
         if(i <  10){
-            chunk = chunk + " <div class='mySlides'> <div class='numbertext'>" + i.toString() +  "/ 33</div> <img src=" + path + "0" + i.toString() + ".jpg> </div>";            
+            chunk = chunk + " <div class='mySlides'> <div class='numbertext'>" + i.toString() +  "/ " + numOfImages.toString + "</div> <img src=" + imagePath + "0" + i.toString() + ".jpg> </div>";            
         }
         else{
-            chunk = chunk + " <div class='mySlides'> <div class='numbertext'>" + i.toString() +  " / 33</div> <img src=" + path + i.toString() + ".jpg> </div>";
+            chunk = chunk + " <div class='mySlides'> <div class='numbertext'>" + i.toString() +  " / " + numOfImages.toString + "</div> <img src=" + imagePath + i.toString() + ".jpg> </div>";
         }
     }
     modal.innerHTML = chunk + currentHTML;
@@ -26,10 +30,10 @@ function rightbttn(){
         index = 1;
     for(var i =0; i < imgArray.length; i++){
         if(index < 10){
-            imgArray[i].src = imagePath + "0" + index.toString() +"-min.jpg"
+            imgArray[i].src = thumbnailPath + "0" + index.toString() +"-min.jpg"
         }  
         else{
-            imgArray[i].src = imagePath + index.toString() +"-min.jpg"
+            imgArray[i].src = thumbnailPath + index.toString() +"-min.jpg"
         }
         index++;
         if(index == numOfImages+1){
@@ -61,10 +65,10 @@ function leftbttn(){
     }
     for(var i =0; i < imgArray.length; i++){
         if(index < 10){
-            imgArray[i].src = imagePath + "0" + index.toString() +"-min.jpg"
+            imgArray[i].src = thumbnailPath + "0" + index.toString() +"-min.jpg"
         }  
         else{
-            imgArray[i].src = imagePath + index.toString() +"-min.jpg"
+            imgArray[i].src = thumbnailPath + index.toString() +"-min.jpg"
         }
         index++;
         if(index == numOfImages+1)
