1d789301fdc65a2efdffdaf576e6e4921dad5c99
mmerendino@fordham.edu <mmerendino@fordham.edu>
2017-10-24 13:04:34 -0400

    Updates to the illustration page. Used a bootstrap modal instead of HTML

--- scripts/illustration.js
+++ scripts/illustration.js
@@ -1,55 +1,30 @@
-var numOfImages = 33
+//JavaScript File for the Illustration page itself
+//JavaScript for the  popup Modal is under Lightbox.js
 
+//number of images in the folder. YOU MUST UPDATE
+var numOfImages = 33
+//Array of all 6 images on the illustration page. 
 var imgArray = [document.getElementById("i1"), document.getElementById("i2"), 
 document.getElementById("i3"), document.getElementById("i4"), document.getElementById("i5"),
 document.getElementById("i6")]
+//Path of all images
 var imagePath = "images/Illustration/thumbnails/Illustrate_"
+//path of all thumbnails
 var thumbnailPath = "images/Illustration/thumbnails/Illustrate_"
+//Index of the next pages first image. 
+//So this starts on the index of the first image on page 2
 var index = 7;
-var modal = document.getElementById("popupContent")
 
-window.onload = function load(){
-    loadAllSlides();
-}
-
-function loadAllSlides(){
-    var chunk = "";
-    var currentHTML = modal.innerHTML;
-    for(var i = 1; i < numOfImages+1; i++){
-        if(i <  10){
-            chunk = chunk + " <div class='mySlides'> <div class='numbertext'>" + i.toString() +  "/ " + numOfImages.toString() + "</div> <img src=" + imagePath + "0" + i.toString() + "-min.jpg> </div>";            
-        }
-        else{
-            chunk = chunk + " <div class='mySlides'> <div class='numbertext'>" + i.toString() +  " / " + numOfImages.toString() + "</div> <img src=" + imagePath + i.toString() + "-min.jpg> </div>";
-        }
-    }
-    modal.innerHTML = chunk + currentHTML;
-}
-function rightbttn(){
-    if(index >= numOfImages)
-        index = 1;
-    for(var i =0; i < imgArray.length; i++){
-        if(index < 10){
-            imgArray[i].src = thumbnailPath + "0" + index.toString() +"-min.jpg"
-        }  
-        else{
-            imgArray[i].src = thumbnailPath + index.toString() +"-min.jpg"
-        }
-        index++;
-        if(index == numOfImages+1){
-            index = 1; 
-        }
-    }
-    setonclick()
-}
+//Sets the slide number when you click the image. 
 function setonclick(){
-    imgArray[0].onclick = function onclick(event) { openModal(); currentSlide(pullNum(imgArray[0].src)) }        
-    imgArray[1].onclick = function onclick(event) { openModal(); currentSlide(pullNum(imgArray[1].src)) }        
-    imgArray[2].onclick = function onclick(event) { openModal(); currentSlide(pullNum(imgArray[2].src)) }      
-    imgArray[3].onclick = function onclick(event) { openModal(); currentSlide(pullNum(imgArray[3].src)) }    
-    imgArray[4].onclick = function onclick(event) { openModal(); currentSlide(pullNum(imgArray[4].src)) }       
-    imgArray[5].onclick = function onclick(event) { openModal(); currentSlide(pullNum(imgArray[5].src)) } 
+    imgArray[0].onclick = function onclick(event) {currentSlide(pullNum(imgArray[0].src)) }        
+    imgArray[1].onclick = function onclick(event) {currentSlide(pullNum(imgArray[1].src)) }        
+    imgArray[2].onclick = function onclick(event) {currentSlide(pullNum(imgArray[2].src)) }      
+    imgArray[3].onclick = function onclick(event) {currentSlide(pullNum(imgArray[3].src)) }    
+    imgArray[4].onclick = function onclick(event) {currentSlide(pullNum(imgArray[4].src)) }       
+    imgArray[5].onclick = function onclick(event) {currentSlide(pullNum(imgArray[5].src)) } 
 }
+//Pulls the number of the image from the source string
 function pullNum(src){
     var num;
     var str;
@@ -58,6 +33,7 @@ function pullNum(src){
     num = parseInt(str);    
     return num;
 }
+//When the left button is clicked. 
 function leftbttn(){
     index = index - 12;
     if(index <= 0){
@@ -75,4 +51,22 @@ function leftbttn(){
             index = 1;
     }
     setonclick();
+}
+//If Right button is clicked
+function rightbttn(){
+    if(index >= numOfImages)
+        index = 1;
+    for(var i =0; i < imgArray.length; i++){
+        if(index < 10){
+            imgArray[i].src = thumbnailPath + "0" + index.toString() +"-min.jpg"
+        }  
+        else{
+            imgArray[i].src = thumbnailPath + index.toString() +"-min.jpg"
+        }
+        index++;
+        if(index == numOfImages+1){
+            index = 1; 
+        }
+    }
+    setonclick()
 }
--- scripts/lightbox.js
+++ scripts/lightbox.js
@@ -1,10 +1,4 @@
-function openModal() {
-  document.getElementById('myModal').style.display = "block";
-}
-
-function closeModal() {
-  document.getElementById('myModal').style.display = "none";
-}
+var imagePath = "images/Illustration/Illustrate_"
 
 var slideIndex = 1;
 showSlides(slideIndex);
@@ -18,19 +12,116 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  var img = document.getElementById('MainImg')
+  if (n < 10)
+    img.src = imagePath + "0" + n + ".jpg"
+  else
+    img.src = imagePath + n + ".jpg"
+  setText(n);
+}
+function setText(n){
+  var title = document.getElementById('imgTitle')
+  switch(n) {
+    case 1:
+      title.innerHTML = "<i>Charlize</i> (Graphite)"
+      break;
+    case 2:
+      title.innerHTML = "<i>Drewys Room</i> (Charcoal, pastel, gouache)"
+      break;
+    case 3:
+      title.innerHTML = "<i>Drewys Room</i> (Graphite)"
+      break;
+    case 4:
+      title.innerHTML = "<i>Pieces of Drewy</i> (Graphite & Ink)"
+      break;
+    case 5:
+      title.innerHTML = "<i>Andy, Self-Portrait</i> (Acrylic paint & colored pencil)"
+      break;
+    case 6:
+      title.innerHTML = "<i>Birth Book</i> (Water Color)"
+      break;
+    case 7:
+      title.innerHTML = "<i>Collage of Art Room</i> (Acrylic paint & Canson paper)"
+      break;
+    case 8:
+      title.innerHTML = "<i>Pride</i> (Graphite)"
+      break;
+    case 9:
+      title.innerHTML = "<i>Pride Continued...</i> (Acrylic paint & gouache)"
+      break;
+    case 10:
+      title.innerHTML = "<i>Pride Continued...</i> (Acrylic paint & gouache)"
+      break;
+    case 11:
+      title.innerHTML = "<i>Pride Continued...</i> (Acrylic paint & gouache)"
+      break;
+    case 12:
+      title.innerHTML = "<i>Dinnertime</i> (Bedsheet, pillowcases, ink, acrylic paint)"
+      break;
+    case 13:
+      title.innerHTML = "<i>Pearl</i> (Gouache & graphite)"
+      break;
+    case 14:
+      title.innerHTML = "<i>Charcoal Smear I</i> (Ink & Graphite)"
+      break;
+    case 15:
+      title.innerHTML = "<i>Charcoal Smear II</i> (Ink & Graphite)"
+      break;
+    case 16:
+      title.innerHTML = "<i>Space Dolphins I</i> (Ink)"
+      break;
+    case 17:
+      title.innerHTML = "<i>Space Dolphins II</i> (Ink)"
+      break;
+    case 18:
+      title.innerHTML = "<i>Easter Series</i> (Ink On Photograph)"
+      break;
+    case 19:
+      title.innerHTML = "<i>Easter Series</i> (Ink On Photograph)"
+      break;
+    case 20:
+      title.innerHTML = "<i>Easter Series</i> (Ink On Photograph)"
+      break;
+    case 21:
+      title.innerHTML = "<i>Easter Series</i> (Ink On Photograph)"
+      break;
+    case 22:
+      title.innerHTML = "<i>Easter Series</i> (Ink On Photograph)"
+      break;
+    case 23:
+      title.innerHTML = "<i>Easter Series</i> (Ink On Photograph)"
+      break;
+    case 24:
+      title.innerHTML = "<i>Deconstructing Keating</i> (Gouache & graphite)"
+      break;
+    case 25:
+      title.innerHTML = "<i>Headphones</i> (Newsprint, gouache, graphite)"
+      break;
+    case 26:
+      title.innerHTML = "<i>Megan</i> (Acrylic paint on VHS still)"
+      break;
+    case 27:
+      title.innerHTML = "<i>Art Room Collage</i> (Acrylic paint)"
+      break;
+    case 28:
+      title.innerHTML = "<i>Rosalyn</i> (Colored pencil)"
+      break;
+    case 29:
+      title.innerHTML = "<i>Vivien</i> (Graphite)"
+      break;
+    case 30:
+      title.innerHTML = "<i>Judy</i> (Graphite)"
+      break;
+    case 31:
+      title.innerHTML = "<i>Memehouse</i> (Gouache & graphite)"
+      break;
+    case 32:
+      title.innerHTML = "<i>Passport</i> (Illustration board & gouache)"
+      break;
+    case 33:
+      title.innerHTML = "<i>Plant</i> (Newsprint & graphite)"      
+      break;
+    default:
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+
 }
